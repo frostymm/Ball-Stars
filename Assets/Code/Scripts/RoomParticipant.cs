593bfba585af56cbd19b9a6525d23e58bccdfb6b
Michael McMurdy <snowwboi@gmail.com>
2015-04-23 16:35:10 -0400

    Most recent version

---
+++ Assets/Code/Scripts/RoomParticipant.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RoomParticipant : MonoBehaviour {
+
+	public Text playerText;
+	public Image readyImage;
+	public PhotonPlayer player;
+
+	// Use this for initialization
+	void Start () {
+		if(player.name != "")
+			playerText.text = player.name;
+		else
+			playerText.text = "NoName";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!player.isMasterClient)
+		{
+			if((bool)player.customProperties["Ready"])
+				readyImage.color = Color.green;
+			else
+				readyImage.color = Color.gray;
+		}
+		else
+		{
+			readyImage.color = Color.blue;
+		}
+	}
+}
---
+++ Assets/Code/Scripts/ServerObject.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ServerObject : MonoBehaviour {
+
+	public Button roomJoinButton;
+	public Text roomNameText;
+
+	public RoomInfo room;
+
+	public void JoinRoom()
+	{
+		NetworkManager.Instance().JoinRoom(room);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	private bool isFull = false;
+	// Update is called once per frame
+	void Update () {
+		if(room != null)
+		{
+			if(room.open)
+			{
+				if(room.playerCount >= room.maxPlayers)
+				{
+					if(roomJoinButton.IsInteractable())
+						roomJoinButton.interactable = false;
+
+					isFull = true;
+				}
+				else
+				{
+					if(!roomJoinButton.IsInteractable())
+						roomJoinButton.interactable = true;
+
+					isFull = false;
+				}
+
+
+				if(isFull == false)
+				{
+					if(roomNameText.text != room.name)
+						roomNameText.text = room.name;
+				}
+				else
+				{
+					string newRoomName = room.name + " (Full)";
+					if(roomNameText.text != newRoomName)
+						roomNameText.text = newRoomName;
+				}
+			}
+			else
+			{
+				if(roomJoinButton.IsInteractable())
+					roomJoinButton.interactable = false;
+
+				string newRoomName = room.name + " (In Progress)";
+				if(roomNameText.text != newRoomName)
+					roomNameText.text = newRoomName;
+			}
+		}
+	
+	}
+}
